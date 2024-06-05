175fa74254a4ea7db60af324a34679e2f900500d
Fran_dpj <fdelpinojimenez@gmail.com>
2024-06-05 12:21:56 +0200

    config

--- database/config.js
+++ database/config.js
@@ -1,20 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require( 'mongoose' );
+
 
 const dbConnection = async () => {
-    try {
 
-        await mongoose.connect(process.env.DB_CNN);
+  try {
 
-        console.log('DB online');
-        
-    } catch (error) {
+    await mongoose.connect( process.env.DB_CNN, {
+      // useNewUrlParser: true, 
+      // useUnifiedTopology: true,
+      // useCreateIndex: true
+    } );
 
-        console.log(error);
-        throw new Error('Error a la hora de inicializar BD')
+    console.log( 'DB Online' );
 
-    }
-}
+
+  } catch ( error ) {
+    console.log( error );
+    throw new Error( 'Error a la hora de inicializar BD' );
+  }
+
+
+};
+
 
 module.exports = {
-    dbConnection,
-}
+  dbConnection
+};
